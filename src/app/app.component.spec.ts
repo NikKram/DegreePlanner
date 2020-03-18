84784153af35dee88fcc8f55fe839c74ef015733
Benjamin <benjaminclark964@u.boisestate.edu>
2020-03-17 21:25:34 -0600

    Added buttons and color to landing page

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'DegreePlanner'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('DegreePlanner');
+    expect(app.title).toEqual('Degree Planner');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to DegreePlanner!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Degree Planner!');
   });
 });
